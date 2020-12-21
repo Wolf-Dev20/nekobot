2fec82f6d66d64ef5d251a8eb6232238436059d4
sarok <70753730+sarokshelby1@users.noreply.github.com>
2020-12-22 01:17:39 +0300

    Update lyrics.js

--- Music/lyrics.js
+++ Music/lyrics.js
@@ -1,61 +1,61 @@
 ////////////////////////////
 //////CONFIG LOAD///////////
 ////////////////////////////
-const createBar = require("string-progressbar");
+const { canModifyQueue } = require("../util/MilratoUtil");
 const { Client, Collection, MessageEmbed } = require("discord.js");
+const lyricsFinder = require("lyrics-finder");
 const { attentionembed } = require("../util/attentionembed");
 const { PREFIX } = require(`../config.json`);
 ////////////////////////////
 //////COMMAND BEGIN/////////
 ////////////////////////////
 module.exports = {
-  name: "nowplaying",
-  aliases: ['np',"now-playing","current","current-song"],
-  description: "Show current song",
-  cooldown: 5,
-  edesc: `Type nowplaying in chat, to see which song is currently playing! As well as how long it will take until its finished\nUsage: ${PREFIX}nowplaying`,
+  name: "lyrics",
+  aliases: ["ly", "text"],
+  description: "Get lyrics for the currently playing song",
+  cooldown: 7.5,
+  edesc: `Type the Command while listening to a song, to get the lyrics from!\nUsage: ${PREFIX}lyrics`,
 
-execute(message) {
-    //if not in a guild return
+async execute(message) {
+    //if not in a Guild return
     if(!message.guild) return;
     //react with approve emoji
-    message.react("769665713124016128")
-    //get Server Queue
+    message.react("769665713124016128").catch(console.error);
+    //Get the current Queue
     const queue = message.client.queue.get(message.guild.id);
-    //if nothing playing error
-    if (!queue) return attentionembed(message, "There is nothing playing.").catch(console.error);
-    //Define the current song
-    const song = queue.songs[0];
-    //get current song duration in s
-    let minutes = song.duration.split(":")[0];
-    let seconds = song.duration.split(":")[1];
-    let ms = (Number(minutes)*60+Number(seconds));
-    //get thumbnail
-    let thumb;
-    if (song.thumbnail === undefined) thumb = "https://cdn.discordapp.com/attachments/748095614017077318/769672148524335114/unknown.png";
-    else thumb = song.thumbnail.url;
-    //define current time
-    const seek = (queue.connection.dispatcher.streamTime - queue.connection.dispatcher.pausedTime) / 1000;
-    //define left duration
-    const left = ms - seek;
-    //define embed
-    let nowPlaying = new MessageEmbed()
-      .setTitle("<:Playing:769665713124016128> Now playing")
-      .setDescription(`[**${song.title}**](${song.url})`)
-      .setThumbnail(song.thumbnail.url)
-      .setColor("#c219d8")
-      .setFooter("Time Remaining: " + new Date(left * 1000).toISOString().substr(11, 8));
-      //if its a stream
-      if(ms >= 10000) {
-        nowPlaying.addField("\u200b", "🔴 LIVE", false);
-        //send approve msg
-        return message.channel.send(nowPlaying);
-      }
-      //If its not a stream
-      if (ms > 0 && ms<10000) {
-        nowPlaying.addField("\u200b", "**[" + createBar((ms == 0 ? seek : ms), seek, 25, "▬", "<:currentposition:770098066552258611>")[0] + "]**\n**" + new Date(seek * 1000).toISOString().substr(11, 8) + " / " + (ms == 0 ? " ◉ LIVE" : new Date(ms * 1000).toISOString().substr(11, 8))+ "**" , false );
-        //send approve msg
-        return message.channel.send(nowPlaying);
-      }
+    //If no Queue Error
+    if (!queue) return attentionembed(message, "There is nothing playing");
+    //If not in a VOICE
+    if (!canModifyQueue(message.member)) return;
+    //Set lyrics to null for the try catch
+    let lyrics = null;
+    //define the temporary Embed
+    let temEmbed = new MessageEmbed()
+    .setAuthor("Searching...", "https://cdn.discordapp.com/emojis/757632044632375386.gif?v=1").setFooter("Lyrics")
+    .setColor("#f300e5")
+    //send it and safe it in a variable
+    let result = await message.channel.send(temEmbed)
+    //try to find lyrics
+    try {
+      //use lyricsfinder
+      lyrics = await lyricsFinder(queue.songs[0].title,"");
+      //If no Lyrics define no lyrics
+      if (!lyrics) lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+    }
+    //catch any error
+    catch (error) {
+      lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+    }
+    //define lyrics Embed
+    let lyricsEmbed = new MessageEmbed()
+      .setTitle("<:lyrics:769938447279456296> Lyrics")
+      .setDescription(lyrics)
+      .setColor("#f300e5")
+    //if to long make slice it
+    if (lyricsEmbed.description.length >= 2048)
+      //slice the embed description and redefine it
+      lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
+      //edit to approve
+    return result.edit(lyricsEmbed).catch(console.error);
   }
 };
